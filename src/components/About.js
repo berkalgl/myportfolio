434839f8c0f0364b20ee30fe75f39ded9ad1c787
Berk Algül <berkalgull@gmail.com>
2022-02-03 00:48:47 +0300

    Cv and mail me buttons add

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import { Icon } from "@iconify/react";
 import '../scss/about.scss';
 
@@ -27,7 +28,33 @@ function About(props) {
                         </span>
                     </div>
                 </div>
-
+                <div className="col-md-6">
+                    <div className="col-md-12">
+                        <div className="card">
+                            <div className="card-header">
+                                <div className="icon-group">
+                                    <Icon icon="emojione:red-circle" />
+                                    <Icon icon="twemoji:yellow-circle" />
+                                    <Icon icon="twemoji:green-circle" />
+                                </div>
+                            </div>
+                            <div className="card-body font-trebuchet text-justify ml-3 mr-3">
+                                <span className="wave">{resumeData.basic_info.description_header} 👋 </span>
+                                <p>{resumeData.basic_info.description}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-12 cv-button-group">
+                        <Button href={"/ResumeFolder/"+sharedData.basic_info.resume} className="cv-button" variant="outlined" target='_blank'>
+                            <Icon icon="carbon:document" width="25" />
+                            <small>Download my CV</small>
+                        </Button>
+                        <Button href={"mailto:"+sharedData.basic_info.email} className="cv-button" variant="outlined" target='_blank'>
+                            <Icon icon="bx:bx-mail-send" width="25" />
+                            <small>Mail me</small>
+                        </Button>
+                    </div>
+                </div>
             </div>
         </section>
     )
